f1bdb505ee836f88df2ab166b536b61de8a16026
Kyle <kylechess8@gmail.com>
2021-09-17 18:22:17 -0700

    Removing stuff from built in template

--- Source/src/main/java/com/flamebom/hellfall/HellFall.java
+++ Source/src/main/java/com/flamebom/hellfall/HellFall.java
@@ -1,63 +1,33 @@
 package com.flamebom.hellfall;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 @Mod("hellfall")
-public class HellFall
-{
-    private static final Logger LOGGER = LogManager.getLogger();
+public class HellFall {
+	private static final Logger LOGGER = LogManager.getLogger();
+	public static final String MODID = "hellfall";
 
-    public HellFall() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+	public HellFall() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
+	private void setup(final FMLCommonSetupEvent event) {
+	}
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
+	@SubscribeEvent
+	public void onServerStarting(FMLServerStartingEvent event) {
+	}
 
-    private void processIMC(final InterModProcessEvent event)
-    {
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.messageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-
-        LOGGER.info("HELLO from server starting");
-    }
-
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents {
+	}
 }
