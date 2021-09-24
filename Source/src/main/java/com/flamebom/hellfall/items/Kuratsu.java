a75f0533265a705f762a04c4fa7bc81d260460cb
Kyle <kylechess8@gmail.com>
2021-09-23 19:58:40 -0700

    Damage modifier test

--- Source/src/main/java/com/flamebom/hellfall/items/Kuratsu.java
+++ Source/src/main/java/com/flamebom/hellfall/items/Kuratsu.java
@@ -1,12 +1,21 @@
 package com.flamebom.hellfall.items;
 
+import java.util.HashMap;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.NbtComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -17,36 +26,48 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public class Kuratsu extends SwordItem {
 
-	 public Kuratsu() {
-	        super(Tiers.NETHERITE, 1, -2.8F, new Item.Properties());
-	    }
-	 
-	 @Override
+	public Kuratsu() {
+		super(Tiers.NETHERITE, 1, -2.8F, new Item.Properties());
+	}
+
+	@Override
 	public boolean canDisableShield(ItemStack stack, ItemStack shield, LivingEntity entity, LivingEntity attacker) {
 		return true;
 	}
-	 @Override
+
+	@Override
 	public boolean canBeDepleted() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	 
-		@Override
-		public MutableComponent getName(ItemStack stack) {
-			return new TranslatableComponent(this.getDescriptionId(stack)).withStyle(ChatFormatting.DARK_RED);
-		}
 
+	@Override
+	public MutableComponent getName(ItemStack stack) {
+		return new TranslatableComponent(this.getDescriptionId(stack)).withStyle(ChatFormatting.DARK_RED);
+	}
 
-	 @Override
-		public boolean onDroppedByPlayer(ItemStack item, Player player) {
-		CompoundTag tag =item.getOrCreateTag();
-	
-			return super.onDroppedByPlayer(item, player);
+	@Override
+	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+		System.out.println("test");
+		Multimap<Attribute, AttributeModifier> multimap = HashMultimap.create();
+		if (slot == EquipmentSlot.MAINHAND) {
+			multimap.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier",
+					10, AttributeModifier.Operation.ADDITION));
 		}
-	 @Override
+		return multimap;
+	}
+
+	@Override
+	public boolean onDroppedByPlayer(ItemStack item, Player player) {
+		//CompoundTag tag = item.getOrCreateTag();
+//tag.putInt(getDescriptionId(), 1);
+		return super.onDroppedByPlayer(item, player);
+	}
+
+	@Override
 	public boolean canAttackBlock(BlockState p_43291_, Level p_43292_, BlockPos p_43293_, Player p_43294_) {
 		// TODO Auto-generated method stub
 		return true;
 	}
-	 
+
 }
