62b1f60b3f59d12be0cd3d24a42773823abbd50b
Kyle <kylechess8@gmail.com>
2021-11-08 18:01:49 -0800

    Building my zenith blade

--- HellFall/src/main/java/com/flamebom/hellfall/items/Kuratsu.java
+++ HellFall/src/main/java/com/flamebom/hellfall/items/Kuratsu.java
@@ -3,10 +3,14 @@ package com.flamebom.hellfall.items;
 import java.util.List;
 
 import com.flamebom.hellfall.HellFall;
+import com.flamebom.hellfall.network.PacketHandler;
+import com.flamebom.hellfall.network.PacketLeftClick;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import com.mojang.realmsclient.dto.RealmsServer.WorldType;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
@@ -27,14 +31,46 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.PlaySoundAtEntityEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class Kuratsu extends SwordItem {
 
 	public Kuratsu() {
 		super(Tiers.NETHERITE, 1, -2.8F, new Item.Properties().tab(HellFall.ITEM_GROUP));
+		MinecraftForge.EVENT_BUS.addListener(this::leftClick);
+		MinecraftForge.EVENT_BUS.addListener(this::attackEntity);
 	}
-	
+
+	/*
+	 * Projectile based off Terrablade code
+	 */
+	private void leftClick(PlayerInteractEvent.LeftClickEmpty evt) {
+		if (!evt.getItemStack().isEmpty()
+				&& evt.getItemStack().getItem() == this) {
+			PacketHandler.sendToServer(new PacketLeftClick());
+		}
+	}
+
+	private void attackEntity(AttackEntityEvent evt) {
+		if (!evt.getPlayer().level.isClientSide) {
+			trySpawnSword(evt.getPlayer());
+		}
+	}
+
+	public void trySpawnSword(Player player) {
+		if (!player.getMainHandItem().isEmpty()
+				&& player.getMainHandItem().getItem() == this
+				&& player.getAttackStrengthScale(0) == 1) {
+			EntitySword sword = getSword(player, player.getMainHandItem());
+			player.level.addEntity(sword);
+			//wip
+			player.level.playSound(null, player.getX(), player.getY(), player.getZ(),  null, null, 0.4F, 1.4F);
+		}
+	}
+
 	@Override
 	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
 		CompoundTag tag = stack.getOrCreateTag();
@@ -50,7 +86,7 @@ public class Kuratsu extends SwordItem {
 	@Override
 	public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 		if (attacker instanceof Player) {
-			damageupdater(stack,target,attacker);
+			damageupdater(stack, target, attacker);
 		}
 		return super.hurtEnemy(stack, target, attacker);
 	}
@@ -58,7 +94,7 @@ public class Kuratsu extends SwordItem {
 	public void damageupdater(ItemStack sword, LivingEntity killedEntity, LivingEntity player) {
 		CompoundTag tag = sword.getOrCreateTag();
 		if (killedEntity.getType() == EntityType.SKELETON && killedEntity.isDeadOrDying()) {
-			
+
 			if (tag.contains("currentdamage")) {
 				tag.putInt("currentdamage", 10 + tag.getInt("currentdamage"));
 			} else {
@@ -74,7 +110,6 @@ public class Kuratsu extends SwordItem {
 
 	@Override
 	public boolean canBeDepleted() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -85,7 +120,6 @@ public class Kuratsu extends SwordItem {
 
 	@Override
 	public boolean canAttackBlock(BlockState p_43291_, Level p_43292_, BlockPos p_43293_, Player p_43294_) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
---
+++ HellFall/src/main/java/com/flamebom/hellfall/network/PacketLeftClick.java
@@ -0,0 +1,28 @@
+ package com.flamebom.hellfall.network;
+
+import java.util.function.Supplier;
+
+import com.flamebom.hellfall.items.Kuratsu;
+import com.flamebom.hellfall.setup.Registration;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.fmllegacy.network.NetworkEvent;
+
+/*
+ From botania github code 
+ */
+
+public class PacketLeftClick {
+	public static void encode(PacketLeftClick msg, FriendlyByteBuf buf) {}
+
+	public static PacketLeftClick decode(FriendlyByteBuf buf) {
+		return new PacketLeftClick();
+	}
+
+	public static void handle(PacketLeftClick msg, Supplier<NetworkEvent.Context> ctx) {
+		if (ctx.get().getDirection().getReceptionSide().isServer()) {
+			ctx.get().enqueueWork(() -> ((Kuratsu) Registration.KURATSU.get()).trySpawnSword(ctx.get().getSender()));
+		}
+		ctx.get().setPacketHandled(true);
+	}
+}
